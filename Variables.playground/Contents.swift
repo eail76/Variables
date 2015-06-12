cb92bc58c1e659814a6d97109b5a77e697f48fd0
eail76 <eail@modelo.edu.mx>
2015-06-12 08:48:01 -0500

    Tarea 3 Variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,17 @@
+var name = "Eduardo"
+var title = "Sr"
+
+var returnValue = title + " " + name
+var returnValue2 = "Hola \(title) \(name)"
+
+
+var strongName:String = "Eduardo"
+
+var score = 125
+var multi = 4
+
+var output = "Your score is: \(score * multi)"
+
+let player = "Eduardo";
+let PUBLIC_DNS = "http://mywebsite.com";
+PUBLIC_DNS + "/api/v0/get/user"
